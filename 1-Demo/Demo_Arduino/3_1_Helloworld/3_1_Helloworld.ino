67da0a13b7f70ceaf533e7b0c5dbd4aa64a93b5b
Guanfu Li <guanfuli@Guanfus-Mac-mini.local>
2025-09-22 16:01:47 +0800

    upload files

---
+++ 1-Demo/Demo_Arduino/3_1_Helloworld/3_1_Helloworld.ino
@@ -0,0 +1,9 @@
+void setup()
+{
+ Serial.begin(115200);
+}
+void loop()/Volumes/SINO/Hardwares/ESP32/JC2432W328/1-Demo/Demo_Arduino/3_4-1_TFT_Rainbow/3_4_TFT_Rainbow
+{
+ Serial.println("HelloWorld");
+ delay(2000);
+}
